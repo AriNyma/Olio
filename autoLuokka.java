74695cc4b11e4faa761257aca2eb311feba993a1
Ari Nyman <e2203051@edu.vamk.fi>
2023-10-07 20:52:14 +0300

    olio konstruct

--- autoLuokka.java
+++ autoLuokka.java
@@ -1,21 +1,48 @@
-public class autoLuokka {
-    private String merkki;
-    private int teho;
-    private int vm;
-
-    public autoLuokka(String merkki, int teho, int vm){
+public class AutoLuokka {
+        //Oliomuuttujat
+        private String merkki;
+        private int vuosimalli;
+        private String kayttovoima;
+        
+        //Konstruktori
+        public AutoLuokka(String merkki, int vuosimalli, String kayttovoima){
         this.merkki = merkki;
-        this.teho = teho;
-        this.vm = vm;
-    }
+        this.vuosimalli = vuosimalli;
+        this.kayttovoima = kayttovoima;
+        }
 
-    public void kaynnista() {
-        System.out.println(merkki + " on käynnissä. Kaikella " +  vm + " vuosimallin " + teho + " hevosvoimalla");
-    } 
+        public AutoLuokka(String merkki, int vuosimalli){
+        this.merkki = merkki;
+        this.vuosimalli = vuosimalli;
+        this.kayttovoima = "Ei tiedossa";
+        }
 
-    public static void main(String[] args){
-        autoLuokka auto1 = new autoLuokka("Mursu", 1, 2000);
+        public AutoLuokka(String merkki){
+        this.merkki = merkki;
+        this.vuosimalli = 0;
+        this.kayttovoima = "Ei tiedossa";
+        }
 
-        auto1.kaynnista();
-    }
+        
+        
+        //Metodit
+        public void kaynnista(){
+            if (vuosimalli == 0 && kayttovoima == null){
+                System.out.println(merkki + " on nyt käynnissä!");
+            } else if (vuosimalli > 0 && kayttovoima == null){
+                System.out.println(merkki + " on nyt käynnissä!(vuosimalli: " + vuosimalli + ")");
+            } else {
+                System.out.println(merkki + " on nyt käynnissä!(vuosimalli: " + vuosimalli + ", käyttövoima: " + kayttovoima + ")");
+            }
+            
+        }
+        
+        public static void main(String[] args){
+            AutoLuokka autoolio = new AutoLuokka("Toyota", 2018, "Bensiini");
+            autoolio.kaynnista();
+            AutoLuokka toinenautoolio = new AutoLuokka("Nissan", 2011);
+            toinenautoolio.kaynnista();
+            AutoLuokka kolmasautoolio = new AutoLuokka("Volkswagen");
+            kolmasautoolio.kaynnista();
+        }
 }
