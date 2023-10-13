e2ca20d8131b2216ee832690725ab04bf3324e52
Ari Nyman <e2203051@edu.vamk.fi>
2023-10-13 19:57:15 +0300

    13.10

---
+++ Elokuva.java
@@ -0,0 +1,54 @@
+//import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Elokuva {
+    private String nimi;
+    private int ikaraja;
+
+    public Elokuva (String elokuvanNimi, int elokuvanIkaraja) {
+        this.nimi = elokuvanNimi;
+        this.ikaraja = elokuvanIkaraja;
+    }
+
+    public String nimi(){
+        return nimi;
+    }
+
+    public int ikaraja(){
+        return ikaraja;
+    }
+    
+    public static void main(String[] args){
+        Scanner lukija = new Scanner(System.in);
+
+        System.out.println("Anna elokuvan nimi:");
+        String nimi = lukija.nextLine();
+
+        System.out.println("Anna elokuvan ikäraja: ");
+        String ikaraja = lukija.nextLine();
+        
+        try {
+            ikaraja != null;
+        }
+        catch (Exception e)
+        {
+            ikaraja = 18;
+        }
+
+        Elokuva elokuva1 = new Elokuva(nimi, ikaraja);
+
+        System.out.println("Minkä ikäinen olet?");
+        int ika = lukija.nextInt();
+
+        elokuva1.nimi();
+        elokuva1.ikaraja();
+
+        if(ika < elokuva1.ikaraja){
+                System.out.println("Et saa katsoa elokuvaa " + elokuva1.nimi);
+        } else {
+                System.out.println("Saat katsoa elokuvaa " + elokuva1.nimi);
+        }
+
+        lukija.close();
+    }
+}
